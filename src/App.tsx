880465a41d661fd97268c7ca0185205d829dacb5
dlaumer <32751207+dlaumer@users.noreply.github.com>
2025-09-25 13:57:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/veloclub-horgen-webshop">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
