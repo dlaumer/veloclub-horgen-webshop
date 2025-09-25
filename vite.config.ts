edfe3f84a5cbe3bdc6fcb7c83b802701b365835c
dlaumer <32751207+dlaumer@users.noreply.github.com>
2025-09-25 13:49:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/veloclub-horgen-webshop/', // <-- repo name with leading & trailing slashes
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
